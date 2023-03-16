2ca66b168b8b287cb2da7e1122442490e08dd95a
kmatsumoto8753 <kmatsumoto8753@gmail.com>
2023-03-16 10:07:42 +0900

    11

--- vendor/symfony/dependency-injection/EnvVarProcessor.php
+++ vendor/symfony/dependency-injection/EnvVarProcessor.php
@@ -168,14 +168,7 @@ class EnvVarProcessor implements EnvVarProcessorInterface
 
             if (false === $env || null === $env) {
                 if (!$this->container->hasParameter("env($name)")) {
-                    try {
-                        //Add your code that may throw an error here.
-                        throw new EnvNotFoundException(sprintf('Environment variable not found: "%s".', $name));
-                    } catch (UserNotFoundException $e) {
-                            newrelic_notice_error($e);
-                            //Handle normally.
-                    }
-                    
+                        throw new EnvNotFoundException(sprintf('Environment variable not found: "%s".', $name));                    
                 }
 
                 $env = $this->container->getParameter("env($name)");
